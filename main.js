6294416e0c65ebb98f8e65b98d270c617f675bbc
raghuraj20 <balasubramaniam.raghuraj@gmail.com>
2020-08-11 12:22:23 +0530

    Funwork 1 at uki

---
+++ main.js
@@ -0,0 +1,21 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+  // soundcloude song id
+  SC.stream('/tracks/298956840',function(sound){
+    // start the music
+    $('#start').click(function(e) {
+           e.preventDefault(); //Don't buuton function
+           sound.start();
+         });
+
+    // stop the music
+    $('#stop').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+  });
+});
